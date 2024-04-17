ad7ecd1b5822c622be9511a614aa76d47804c77c
TylerLo416 <lotyler416@gmail.com>
2024-04-16 18:03:15 -0700

    adding prettier + eslint

---
+++ TECApp/eslint.config.mjs
@@ -0,0 +1,9 @@
+import globals from "globals";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginReactConfig,
+  eslintConfigPrettier//MUST BE LAST!!!!!
+];
