c11ef3a966089ecaf58f9cce9a5476258958a3e9
Azhou2023 <anz008@ucsd.edu>
2024-04-23 19:50:06 -0700

    mobile support

--- TECApp/src/components/WorldMap.jsx
+++ TECApp/src/components/WorldMap.jsx
@@ -1,61 +1,78 @@
 import * as d3 from "d3";
 import React, { useEffect, useState, useRef } from "react";
-import { StyleSheet, Text, View } from 'react-native';
-import data from "../../GeoChart.world.geo.json"
+import { StyleSheet, Text, View, Dimensions, Platform, Pressable } from 'react-native';
+import data from "../../GeoChart.world.geo.json"    
+import Svg, { Path, G } from "react-native-svg"
 
 export const WorldMap = ()=>{
-    const svgRef = useRef();
+    const [web, setWeb] = useState(Platform.OS=="web")
     const wrapperRef = useRef();
 
     const [selectedCountry, setSelectedCountry] = useState();
+    
+    const width = Dimensions.get("window").width;
+    const height = Dimensions.get("window").height;
 
-    useEffect(()=>{
-        const { width, height } = wrapperRef.current.getBoundingClientRect();
-
-        const svg = d3.select(svgRef.current);
-        console.log(width)
-        console.log(height)
-        
-        const projection = d3.geoNaturalEarth1().fitSize([width, height], data);
-        const pathGenerator = d3.geoPath().projection(projection);
-
-        svg.selectAll(".country")
-            .data(data.features)
-            .join("path")
-            .on("click", (event, feature) =>{setSelectedCountry(feature)})
-            .attr("class", "country")
-            .attr("fill", "#c9c9c9")
-            .style("stroke", "white")
-            .style("stroke-width", 0.5)
-            .attr("d", feature => pathGenerator(feature));
-    }, [])
-
-    useEffect(()=>{
-        console.log(selectedCountry!==undefined?selectedCountry.properties.admin:"")
-    }, [selectedCountry])
+    const projection = d3.geoNaturalEarth1().fitSize([width*0.95, height*0.65], data);
+    const pathGenerator = d3.geoPath().projection(projection);
 
     return(
-        <View ref={wrapperRef} style={styles.container}>
-			<svg style={styles.svg} ref={svgRef}></svg>
-            <Text>{selectedCountry!==undefined?selectedCountry.properties.admin:""}</Text>
+      
+        <View ref={wrapperRef} style={web?webStyles.container:mobileStyles.container}>
+			<Svg style={web?webStyles.svg:mobileStyles.svg}>
+                <G>
+                    {data.features.map((feature, index)=>(
+                        web?(
+                        <Path d={pathGenerator(feature)} 
+                                key={index}
+                                stroke="white" 
+                                strokeWidth={0.5} fill="gray"
+                                onClick={()=>{
+                                    setSelectedCountry(feature)}}
+                        ></Path>):
+                        <Path d={pathGenerator(feature)} 
+                                key={index}
+                                stroke="white" 
+                                strokeWidth={0.5} fill="gray"
+                                onPress={()=>{
+                                    setSelectedCountry(feature)}}
+                        ></Path>)
+                    )}
+                </G>
+            </Svg>
+            <Text>{selectedCountry!==undefined?selectedCountry.properties.admin:" "}</Text>
 		</View>
     )
 }
 
-const styles = StyleSheet.create({
+const webStyles = StyleSheet.create({
 	container: {
-        // backgroundColor: "red",
-        height: "95vh",
-        width: "95vw",
+        // backgroundColor: "gray",
+        width: "100%",
+        height: "100%",
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
 	},
     svg: {
-        height: "100%",
+
+        height: "65%",
         width: "100%",
-        
         // backgroundColor: "red",
     }
-    
 });
+
+const mobileStyles = StyleSheet.create({
+    container: {
+        width: "100%",
+        height: "100%",
+        flex: 1,
+        alignItems: "center"
+	},
+    svg: {
+        height: "40%",
+        width: "100%",
+        marginTop: "50%"
+        // marginBottom: "20%"
+    }
+})
