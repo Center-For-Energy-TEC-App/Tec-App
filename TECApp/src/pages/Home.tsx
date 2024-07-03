e2f6dc97decd36e20fab7c1d7f996401618f800b
Yoto Kim <yongtokim1215@gmail.com>
2024-07-03 14:37:09 -0700

    Initial bottom sheet, no bugs

--- TECApp/src/pages/Home.tsx
+++ TECApp/src/pages/Home.tsx
@@ -1,6 +1,23 @@
 import { WorldMap } from '../components/WorldMap'
+import BottomSheet from '@gorhom/bottom-sheet'
 import React from 'react'
+import { useMemo } from 'react'
+import { Text, View } from 'react-native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 export const Home = () => {
-  return <WorldMap />
+  const snapPoints = useMemo(() => ['25%', '50%', '70%'], [])
+
+  return (
+    <View style={{flex:1}}>
+      <WorldMap />
+      <GestureHandlerRootView>
+        <BottomSheet snapPoints={snapPoints}>
+          <View>
+            <Text>This is awesome!</Text>
+          </View>
+        </BottomSheet>
+      </GestureHandlerRootView>
+    </View>
+  )
 }
