fa653db851708f8ba574e0ffbbc35733c75b8305
Azhou2023 <anz008@ucsd.edu>
2024-07-16 16:40:14 -0700

    fixed bottom border + region names

--- TECApp/src/components/DataVisualizations.tsx
+++ TECApp/src/components/DataVisualizations.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import React from 'react'
+import { Text, StyleSheet, View } from 'react-native'
 
 const DataVisualizations = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>
-        Here you can view data visualizations related to the selected country's renewable energy sources.
+        Here you can view data visualizations related to the selected country's
+        renewable energy sources.
       </Text>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -22,6 +23,6 @@ const styles = StyleSheet.create({
     fontWeight: '400',
     fontSize: 14,
   },
-});
+})
 
-export default DataVisualizations;
+export default DataVisualizations
