63147ec1748d28ded95223926068688853d0953e
Yoto Kim <yongtokim1215@gmail.com>
2024-10-04 11:15:12 -0700

    Took a look at bug

--- TECApp/app/_layout.tsx
+++ TECApp/app/_layout.tsx
@@ -2,7 +2,7 @@ import { router, Stack } from 'expo-router'
 import React, { Alert, Pressable, Text } from 'react-native'
 import { HelpButton } from './SVGs/HelpButton'
 import { BackArrow } from './SVGs/BackArrow'
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { GestureHandlerRootView, TapGestureHandler } from 'react-native-gesture-handler'
 
 const RootLayout = () => {
   return (
