9bd8dcfb1a40b9aea1cb5105e43a625734b1d6f1
Azhou2023 <anz008@ucsd.edu>
2024-11-12 19:50:23 -0800

    merge

--- TECApp/app/index.tsx
+++ TECApp/app/index.tsx
@@ -3,7 +3,7 @@ import { Redirect } from 'expo-router'
 import { useFonts } from 'expo-font'
 import * as SplashScreen from 'expo-splash-screen'
 import { checkVersion } from 'react-native-check-version'
-import {Alert, Linking} from "react-native"
+import {Alert, Linking, Platform} from "react-native"
 
 SplashScreen.preventAutoHideAsync()
 
@@ -11,8 +11,6 @@ export default function App() {
   const [loaded, error] = useFonts({
     'Brix Sans': require('../assets/fonts/BrixSans.otf'),
   })
-
-  const [needsUpdate, setNeedsUpdate] = useState<boolean>(true)
 
   const checkNeedsUpdate = async () => {
     const version = await checkVersion({ bundleId: 'com.CER.Tec-App', currentVersion: "1.0.2"})
@@ -24,25 +22,19 @@ export default function App() {
       SplashScreen.hideAsync()
     }
 
-    checkNeedsUpdate().then((version) => {
-      if (version.needsUpdate) {
-        Alert.alert("New Update Available",
-          'Please update your app to the latest available version in the app store!\n',
-          [{text: "Update", onPress: ()=>Linking.openURL("https://apps.apple.com/us/app/triton-energy-climate/id6737405522")}]
-        )
-        setNeedsUpdate(true)
-        return null
-      } else {
-        setNeedsUpdate(false)
+    if(Platform.OS==="ios"){
+      checkNeedsUpdate().then((version) => {
+        if (version.needsUpdate) {
+          Alert.alert("New Update Available",
+            'Please update your app to the latest available version in the app store!\n',
+            [{text: "Update", onPress: ()=>Linking.openURL("https://apps.apple.com/us/app/triton-energy-climate/id6737405522")}]
+          )
+          return null
       }
     })
-  }, [loaded, error])
+  }}, [loaded, error])
 
   if (!loaded && !error) {
-    return null
-  }
-
-  if (needsUpdate) {
     return null
   }
 
